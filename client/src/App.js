c97ea311ebaca77fc3fe2ed8a834702bc60073ab
saswathi p <saswathi.p3@gmail.com>
2023-04-01 04:38:15 +0000

    finished CRUD app

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+//import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import CreateBook from './components/CreateBook';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+import ShowBookList from './components/ShowBookList';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<ShowBookList/>} />
+        <Route path='/create-book' element={ <CreateBook />} />
+        <Route path="/edit-book/:id" element={ <UpdateBookInfo />} />
+        <Route path="/show-book/:id" element={ <ShowBookDetails /> } />
+
+      </Routes>
+    </div>
+  </Router>
+
+  );
+}
+
+export default App;
